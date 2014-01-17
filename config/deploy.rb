01985df68c6703319fab5b45865e5e6578f8ff5c
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-01-18 02:00:36 +0900

    setting modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,10 +63,10 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
+      within release_path do
       #   execute :rake, 'cache:clear'
-      execute :rake, 'assets:precompile'
-      # end
+        execute :rake, 'assets:precompile'
+      end
     end
   end
 
