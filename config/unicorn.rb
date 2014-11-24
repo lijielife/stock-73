3f6a3a77446979b07e81702ebf8ae0f2ee3e5930
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-11-24 14:57:17 +0900

    unicorn setting update

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+worker_processes 2
+listen '/tmp/unicorn.sock'
+pid "/var/www/stock/current/shared/tmp/pids/unicorn.pid"
+
+stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
+stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
+
+preload_app true
