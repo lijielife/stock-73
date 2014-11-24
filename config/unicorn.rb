8ab860eb8823c9535d59c39e3cf5f5670c9ac304
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-11-24 15:23:03 +0900

    unicorn.rb update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 worker_processes 2
 listen '/tmp/unicorn.sock'
-pid "/var/www/stock/current/shared/tmp/pids/unicorn.pid"
+pid "/var/www/stock/shared/tmp/pids/unicorn.pid"
 
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
