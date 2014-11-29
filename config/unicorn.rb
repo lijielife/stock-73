cd5900c9884600ec7e46061cfd6b49f45ae59d40
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-11-30 02:24:08 +0900

    unicorn setting

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,7 @@
 worker_processes 2
-#listen '/tmp/unicorn.sock'
-listen '/var/www/stock/shared/tmp/sockets/unicorn.sock'
+working_directory "#{app_path}/current/"
+listen '/tmp/unicorn.sock'
+#listen '/var/www/stock/shared/tmp/sockets/unicorn.sock'
 pid "/var/www/stock/shared/tmp/pids/unicorn.pid"
 
 stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
