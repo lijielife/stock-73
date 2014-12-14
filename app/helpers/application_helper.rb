7ca7475591e71c4ae09b32b90a994f77cbe8878e
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-12-14 20:50:10 +0900

    bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+
+
+#  # 選択中のサイドメニューのクラスを返す
+#  def sidebar_activate(sidebar_link_url)
+#    current_url = request.headers['PATH_INFO']
+#    if current_url.match(sidebar_link_url)
+#      ' class="active"'
+#    else
+#      ''
+#    end
+#  end
+#
+#  # サイドメニューの項目を出力する
+#  def sidebar_list_items
+#    items = [
+#      {:text => 'Users',      :path => admin_users_path},
+#      {:text => 'Contacts',   :path => admin_contacts_path}
+#    ]
+#
+#    html = ''
+#    items.each do |item|
+#      text = item[:text]
+#      path = item[:path]
+#      html = html + %Q(<li#{sidebar_activate(path)}><a href="#{path}">#{text}</a></li>)
+#    end
+#
+#    raw(html)
+#  end
+
 end
