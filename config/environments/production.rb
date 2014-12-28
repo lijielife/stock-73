ab80c0f44f0dfc1f4b75ad1afbe4d56b56c5282c
K.Yamanaka <melody_breeze86@kjb.biglobe.ne.jp>
2014-12-28 21:37:29 +0900

    assets:precompile deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Stock::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  #config.assets.compile = true    #trueに設定すると動的コンパイルが「有効」
+  #config.assets.compile = false
+  config.assets.compile = true    #trueに設定すると動的コンパイルが「有効」
 
   # Generate digests for assets URLs.
   config.assets.digest = true
